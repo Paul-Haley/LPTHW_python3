5900b719e7c206ec904954ec3d80889400ef1384
Paul Haley <paul.haley@bigpond.com>
2017-02-09 20:36:50 +0800

    Exercise 44 completed, commiting now as next exercise is long.

---
+++ ex44e.py
@@ -0,0 +1,32 @@
+class Other(object):
+
+    def override(self): # This is never called
+        print("OTHER override()")
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILE, BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()
